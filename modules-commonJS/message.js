a2526be5ab6d8bfb69d69928e46a01b35b230d34
Lucy Lainez <156219119+lb229@users.noreply.github.com>
2024-05-09 16:36:14 +0200

    Add files via upload
    add modules common js

---
+++ modules-commonJS/message.js
@@ -0,0 +1,5 @@
+function theMessage (message){
+  console.log(`The message is: ${message}`);
+}
+
+module.exports = theMessage;
---
+++ modules-commonJS/script.js
@@ -0,0 +1,8 @@
+/* Exercise 3 - Create and use CommonJS modules
+
+Create a script that uses module.exports to export a function.
+Create another script that uses require() to import the function and then calls it. */
+
+const theMessage = require("./message.js");
+
+theMessage("Hey the first script")
